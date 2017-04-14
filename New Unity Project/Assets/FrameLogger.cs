0e035cd4ae76b16a17fe5c309ef25c7c1b4a1020
admin\user <xuta.liparteliani@gmail.com>
2017-04-14 23:31:18 +0400

    sisuleleebis damateba

---
+++ New Unity Project/Assets/FrameLogger.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameLogger : MonoBehaviour {
+
+    int count = 0;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // Debug.Log(Time.time);
+        
+      //count = count + 1 ;
+
+        // logshi vnaxulobt mxolod lutsebs
+        //if (count % 2 == 0)
+        //{ Debug.Log(count); }
+        //else
+        // Debug.Log("kenti");*/
+
+        //tsamshi ramdeni kadria
+        //float time1 = Time.time;
+        //Debug.Log(count / Time.time);
+
+        //if (count == 100)
+        //   count = 0;
+        //Debug.Log(count);
+
+
+        /*
+         * count is counting frames
+         * if count reaches 100 start decrmenting frames
+         * if count reaches 0 start incrementing frames */
+        //creating int frameDirection 
+
+       /* int frameCounter = 0;
+        int countDirection = 1;
+
+        if (frameCounter == 100)
+        {
+            countDirection = -1;
+        }
+
+        if (frameCounter == 0);
+        {
+            countDirection = 1 */
+
+        }
+
+    }
+        
+
+}
