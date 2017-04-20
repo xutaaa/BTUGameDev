44d4158ada998f18abdcf8f8a8351c9c35350b3c
admin\user <xuta.liparteliani@gmail.com>
2017-04-20 18:01:47 +0400

    improvments in Scripts (added cube movement)

---
+++ New Unity Project/Assets/GameDev5.cs
@@ -0,0 +1,58 @@
+﻿
+using UnityEngine;
+
+public class GameDev5 : MonoBehaviour {
+
+    int a;
+    float b = 0.653442f;
+    bool c = true;
+    string d = "some text";
+
+    public Vector3 position;
+    public Vector3 moveSpeed;
+    public Vector3 ScaleSpeed;
+    public Vector3 rotSpeed;
+    public string newName;
+    //public Transform otherTransform;
+   
+	
+	void Start ()
+    {
+        //object starts at the position were it last was (transformed)
+        //position = transform.position;
+        //transform.name = "qurdebs sicocxle";
+        //transform.name = newName;
+
+       
+	}
+	
+
+	void Update ()
+    {
+        //position = position + moveSpeed;
+
+        transform.position = (transform.position + moveSpeed);
+
+        if
+            (transform.position.x >= 5f)
+        {
+            moveSpeed = moveSpeed * -1f;
+        }
+        else if 
+            (transform.position.x <= -5f)
+        {
+            moveSpeed = moveSpeed * -1f;
+        }
+        //if (position.y < 10);
+        //speed = -1;
+
+
+        //Debug.Log(position + speed);
+
+        //transform.localScale = (transform.localScale + ScaleSpeed);
+
+        //transform.eulerAngles = (transform.eulerAngles + rotSpeed);
+
+    
+	}
+}
